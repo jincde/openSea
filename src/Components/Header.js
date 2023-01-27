2282f539463567d1f18e6f4992d53c418f64aebe
jincde <asdzxc@kakao.com>
2023-01-28 00:46:28 +0900

    Feat : 내비게이션 바 아이템 추가

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,13 +6,41 @@ function Header() {
     <header className="navbar">
       <div className="navbar-logo">
         <div className="logo">
-          <img src={logo}></img>
+          <img src={logo} alt="logo"></img>
           <div className='brand-name'>OpenSea</div>
         </div>
       </div>
 
-      <div>asd</div>
-    </header>
+      {/* search */}
+      <div className='search-section'>
+        <div className='search-view-toggle'></div>
+        <div className='search-form'>
+          <span class="material-symbols-outlined md-inactive">
+            search
+          </span>
+          <input placeholder='Search items, collections, and accounts' />
+        </div>
+      </div>
+
+      {/* ul list 1 */}
+      <div className='navbar-menu'>
+        <ul>
+          <li>Explore</li>
+          <li>Drops</li>
+          <li>Stats</li>
+          <li>Resources</li>
+        </ul>
+      </div>
+
+      {/* ul list 2 */}
+      <div className='navbar-icons'>
+        <ul>
+          <li>O</li>
+          <li>O</li>
+          <li>O</li>
+        </ul>
+      </div>
+    </header >
   )
 }
 
