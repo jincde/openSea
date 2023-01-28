ac1e93e7f24db4ee50c56866df9f6b9cc279bdb4
jincde <asdzxc@kakao.com>
2023-01-29 00:44:31 +0900

    Style : swiper 카드 디자인

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from "swiper";
+
 import test from '../img/test1.avif';
 
 // Import Swiper styles
@@ -8,25 +10,59 @@ import 'swiper/css';
 export default () => {
   return (
     <Swiper
-      spaceBetween={50}
-      slidesPerView={5}
+      navigation={true}
+      spaceBetween={20}
+      slidesPerView={4}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
       <SwiperSlide>
         <div className="test-card">
-          <div className="test-1"></div>
-          <div className="test-2">ㅁㄴㅇ
-            <img src={test} /></div>
+          <div className="test-2">
+            <img src={test} />
+          </div>
         </div>
       </SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-      <SwiperSlide>Slide 7</SwiperSlide>
-      <SwiperSlide>Slide 8</SwiperSlide>
+
+      <SwiperSlide>
+        <div className="test-card">
+          <div className="test-2">
+            <img src={test} />
+          </div>
+        </div>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <div className="test-card">
+          <div className="test-2">
+            <img src={test} />
+          </div>
+        </div>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <div className="test-card">
+          <div className="test-2">
+            <img src={test} />
+          </div>
+        </div>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <div className="test-card">
+          <div className="test-2">
+            <img src={test} />
+          </div>
+        </div>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <div className="test-card">
+          <div className="test-2">
+            <img src={test} />
+          </div>
+        </div>
+      </SwiperSlide>
     </Swiper>
   );
 };
