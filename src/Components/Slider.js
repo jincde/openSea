c91977403a955321c0a8ef7c9aeaa3351281e980
jincde <asdzxc@kakao.com>
2023-01-29 16:23:47 +0900

    Feat : Swiper 슬라이더 반응형 개선

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -12,6 +12,27 @@ export default () => {
   return (
     <Swiper
       navigation={true}
+      breakpoints={{
+        600: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 40
+        },
+        2256: {
+          slidesPerView: 6,
+          spaceBetween: 40
+        }
+      }
+      }
       modules={[Navigation]}
       spaceBetween={20}
       slidesPerView={4}
