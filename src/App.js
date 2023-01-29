8cddbc1c2f79b6ac23456dc7067327992590bdae
jincde <asdzxc@kakao.com>
2023-01-29 17:09:53 +0900

    Feat : Trending영역 반응형

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss'
 import Header from './Components/Header'
 import Banner from './Components/Banner'
+import Rank from './Components/Rank'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <Rank />
     </div>
   )
 }
---
+++ src/Components/Rank.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+function Rank() {
+
+  return (
+    <section className="rank-container">
+
+      <div className="rank-menu">
+        <div className="rank-menu-left">
+          <p className="menu-content active">Trending</p>
+          <p className="menu-content">Top</p>
+        </div>
+      </div>
+
+      <div className="rank-grid">
+        <div>asd</div>
+        <div>asd</div>
+      </div>
+    </section>
+  )
+}
+
+export default Rank
