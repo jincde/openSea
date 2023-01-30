cc4c469bce7e086e0fb73df3d4d71bf4cd6a1e96
jincde <asdzxc@kakao.com>
2023-01-30 17:26:39 +0900

    Feat : 내브바 dropdown메뉴 추가

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -42,10 +42,52 @@ function Header() {
         {/* ul list 1 */}
         <div className='navbar-menu'>
           <ul>
-            <li>Explore</li>
-            <li>Drops</li>
-            <li>Stats</li>
-            <li>Resources</li>
+            <div className='dropdown'>
+              <li className='main-menu'>Explore</li>
+              <div className='dropdown-menu'>
+                <button>All NFTs</button>
+                <button>Arts</button>
+                <button>Art</button>
+                <button>Collectibles</button>
+                <button>Domain Names</button>
+                <button>Music</button>
+                <button>Photography</button>
+                <button>Sports</button>
+                <button>Trading Cards</button>
+                <button>Utibuttonty</button>
+                <button>Virtual Worlds</button>
+              </div>
+            </div>
+
+            <div className='dropdown'>
+              <li className='main-menu'>Drops</li>
+              <div className='dropdown-menu'>
+                <button>Calendar</button>
+                <button>Learn more</button>
+              </div>
+            </div>
+
+            <div className='dropdown'>
+              <li className='main-menu'>Stats</li>
+              <div className='dropdown-menu'>
+                <button>Rankings</button>
+                <button>Activity</button>
+              </div>
+            </div>
+
+            <div className='dropdown'>
+              <li className='main-menu'>Resources</li>
+              <div className='dropdown-menu'>
+                <button>Learn</button>
+                <button>Help Center</button>
+                <button>Platform Status</button>
+                <button>Partners</button>
+                <button>Taxes</button>
+                <button>Blog</button>
+                <button>Docs</button>
+                <button>Newsletter</button>
+              </div>
+            </div>
           </ul>
         </div>
 
